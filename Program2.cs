36cd6a4ed4e3c6cefc779629be198c1117d4039d
OAlasgarli08 <omarta@code.edu.az>
2023-09-03 21:45:34 +0400

    Created a project but for some reason it doesn't work

---
+++ Program2.cs
@@ -0,0 +1,16 @@
+
+
+Console.WriteLine("Please enter your username: ");
+string username = Console.ReadLine();
+
+Console.WriteLine("Enter your password: ");
+string Password = Console.ReadLine();
+
+if(Password == "Omar070809")
+{
+    Console.WriteLine("Welcome!");
+}
+else
+{
+    Console.WriteLine("!Incorrect username or password!");
+}
